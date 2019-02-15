762d157a82c6af902b2d9954b7327311dd08c1a9
saikumarpuli <saikumarpuli9@gmail.com>
2019-02-15 19:45:44 +0530

    Angular_task2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClassComponent } from './class/class.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
